89a1dc137520908f8de70600b061f24e3a584916
hanaeHb <hanaebertili@gmail.com>
2025-12-28 20:22:26 +0100

    add register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home';
 import { LoginComponent } from './login.component/login.component';
+import {register} from 'node:module';
+import {Register} from './register/register';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent } ,
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: Register }
 
 
 ];
