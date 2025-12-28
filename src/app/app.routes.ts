8f877b64b518e13758de97918a0aebba648d48b1
zahra <zohrajbari2003@gmail.com>
2025-12-28 23:24:54 +0100

    Update app.routes.ts with new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,19 @@ import {Register} from './register/register';
 export const routes: Routes = [
     { path: '', component: HomeComponent } ,
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: Register }
+    { path: 'register', component: Register },
 
+  {
+    path: 'manager',
+    loadComponent: () => import('./components/manager/manager-dashboard/manager-dashboard').then(m => m.ManagerDashboardComponent)
+  },
+  {
+    path: 'client',
+    loadComponent: () => import('./components/client/client-dashboard/client-dashboard').then(m => m.ClientDashboardComponent)
+  },
+
+  // Wildcard route
+  { path: '**', redirectTo: '' }
 ];
+
+
