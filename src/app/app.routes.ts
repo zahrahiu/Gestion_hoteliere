5c1d145b1ec69ccc2fac5cffbf9182475675a02d
hanaeHb <hanaebertili@gmail.com>
2026-01-11 03:43:29 +0100

    fix the merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,13 @@
 // src/app/app.routes.ts
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { LoginComponent } from './login.component/login.component';
-<<<<<<< HEAD
-import {Register} from './register/register';
-import {Catalogue} from './catalogue/catalogue';
-import {ProfileComponent} from '../app/components/client/profil-client/profil-client';
-import {ProfilManager} from './components/manager/profil-manager/profil-manager';
-import {RoomDetails} from './room-details/room-details';
-=======
 import { Register } from './register/register';
 import { Catalogue } from './catalogue/catalogue';
 import { ProfileComponent } from './components/client/profil-client/profil-client';
 import { ProfilManager } from './components/manager/profil-manager/profil-manager';
 import { AdminGuard } from './guards/admin.guard';
->>>>>>> a73d9a14854e1321a9e258ffbd1df2a9760e084c
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home';
+import {LoginComponent} from './login.component/login.component';
+import {RoomDetails} from './room-details/room-details';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,11 +16,8 @@ export const routes: Routes = [
   { path: 'catalogue', component: Catalogue },
   { path: 'catalogue/profil', component: ProfileComponent },
   { path: 'managerprofil', component: ProfilManager },
-<<<<<<< HEAD
   { path: 'room/:id', component: RoomDetails },
 
-=======
-  
   // Route Admin
   {
     path: 'admin',
@@ -55,7 +45,7 @@ export const routes: Routes = [
       }
     ]
   },
->>>>>>> a73d9a14854e1321a9e258ffbd1df2a9760e084c
+
 
   {
     path: 'manager',
