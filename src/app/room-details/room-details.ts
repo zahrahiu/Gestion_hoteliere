d3d7f84bcbdb9d9a4bd2c98df71eda014a957a6f
hanaeHb <hanaebertili@gmail.com>
2026-01-12 20:31:57 +0100

    add reservation

--- src/app/room-details/room-details.ts
+++ src/app/room-details/room-details.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule, NgIf } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { RoomService } from '../services/room.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-room-details',
   standalone: true,
-  imports: [CommonModule, RouterModule, NgIf],
+  imports: [CommonModule, RouterModule, NgIf, FormsModule],
   templateUrl: './room-details.html',
   styleUrls: ['./room-details.css'],
 })
@@ -37,6 +38,10 @@ export class RoomDetails {
         next: (data) => {
           this.room = data;
           this.loading = false;
+
+          // Fill readonly fields in form
+          this.reservation.roomNumber = this.room.numero;
+          this.reservation.roomType = this.room.type;
         },
         error: (err) => {
           console.error(err);
@@ -45,5 +50,39 @@ export class RoomDetails {
         }
       });
     }
+  }
+
+
+
+
+  showModal = false; // <-- Angular variable to control modal
+
+  // Form model
+  reservation = {
+    fname: '',
+    lname: '',
+    email: '',
+    tel: '',
+    roomNumber: '',
+    roomType: '',
+    startDate: '',
+    endDate: '',
+    dob: '',
+    cni: ''
+  };
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  submitReservation() {
+    console.log('Reservation data:', this.reservation);
+    // hna t9dar tsift l-backend via RoomService
+    alert('Reservation confirmed!');
+    this.closeModal();
   }
 }
